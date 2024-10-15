1b08da35657d9c1c9cd80aa83497122ba5ce0b1e
praveen <kpkeee44@gmail.com>
2024-10-15 21:06:43 +0530

    runner edited

--- src/main/java/com/praveenit/runner/RunnerInfo.java
+++ src/main/java/com/praveenit/runner/RunnerInfo.java
@@ -22,6 +22,7 @@ public class RunnerInfo implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 	int i=10;
+int k=11;
 	
 		Pageable pageable= PageRequest.of(2, 5);
 		Page<Employee> page=emprepo.findAll(pageable);
